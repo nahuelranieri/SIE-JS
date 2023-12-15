56e4787222bc6bfde4a63e16d0221c2aeb7deb7b
nahuelranieri <nnranieri.ok@gmail.com>
2023-12-15 16:44:47 -0300

    feat: edit page

--- src/components/table/Edit.jsx
+++ src/components/table/Edit.jsx
@@ -1,12 +1,69 @@
-import { Container, Stack, Typography } from '@mui/material'
-import React from 'react'
+import { Save } from '@mui/icons-material'
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Divider,
+  Grid,
+  Stack,
+  Typography,
+  TextField,
+  Button,
+  Box,
+} from '@mui/material'
+import { React, useRef, useState } from 'react'
 
 const edit = () => {
+ 
+
   return (
-    <Container>
-      <Stack>
-        <Typography>Tu datos</Typography>
-      </Stack>
+    <Container sx={{ my: 1 }}>
+      <Card>
+        <CardContent>
+          <Typography variant='h6'> Edita tus datos</Typography>
+          <Divider sx={{ mb: 1 }} />
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <TextField
+                label="Texto"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                label="Texto"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                label="Texto"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                label="Texto"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                label="Texto"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+          <Box sx={{display:'flex', justifyContent:'center', mt: 2}}>
+            <Button
+              variant="contained"
+              endIcon={<Save />}>
+              Guardar Datos
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
     </Container>
   )
 }
--- src/components/table/TableComponent.jsx
+++ src/components/table/TableComponent.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { DataGrid } from '@mui/x-data-grid';
 import { Visibility, Edit } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
+import randomData from './randomData.json'
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -29,23 +30,12 @@ const columns = [
   {
     field: 'created_at',
     headerName: 'Creado',
-    type: 'dateTime',
     width: 90,
   },
   {
     field: 'updated_at',
     headerName: 'Actualizado',
-    type: 'dateTime',
     width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
   },
   {
     field: 'actions',
@@ -69,17 +59,7 @@ const columns = [
   }
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+const rows = randomData
 
 export const TableComponent = () => {
   
